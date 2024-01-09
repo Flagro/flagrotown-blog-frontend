ddff4af8592d5d62d545a89e9108f1b1bd8bc7d0
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-09 03:13:43 +0100

    eslint prettier fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,38 @@
 <script>
-    import { onMount } from 'svelte';
+	import { onMount } from 'svelte';
 
-    let randomEmoji = '';
+	let randomEmoji = '';
 
-    const getRandomEmoji = () => {
-        const emojis = ['🔥', '💡', '🚀', '🎉', '🔨'];
-        const randomIndex = Math.floor(Math.random() * emojis.length);
-        randomEmoji = emojis[randomIndex];
-    };
+	const getRandomEmoji = () => {
+		const emojis = ['🔥', '💡', '🚀', '🎉', '🔨'];
+		const randomIndex = Math.floor(Math.random() * emojis.length);
+		randomEmoji = emojis[randomIndex];
+	};
 
-    onMount(() => {
-        getRandomEmoji();
-    });
+	onMount(() => {
+		getRandomEmoji();
+	});
 </script>
 
-<style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        background-color: black;
-    }
-
-    .sign {
-        font-size: 48px;
-        font-weight: bold;
-        color: white;
-        background-color: rgba(0, 0, 0, 0.5);
-        padding: 20px;
-    }
-</style>
-
 <div class="container">
-    <div class="sign">{randomEmoji} BLOG IN PROGRESS {randomEmoji}</div>
+	<div class="sign">{randomEmoji} BLOG IN PROGRESS {randomEmoji}</div>
 </div>
 
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		background-color: black;
+	}
+
+	.sign {
+		font-size: 48px;
+		font-weight: bold;
+		color: white;
+		background-color: rgba(0, 0, 0, 0.5);
+		padding: 20px;
+	}
+</style>
