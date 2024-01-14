a7fa1811c9052da6eb0769a8c4d6dbdcc51b0f79
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-14 21:14:33 +0100

    auth store formatting update

--- src/lib/stores/authStore.ts
+++ src/lib/stores/authStore.ts
@@ -2,26 +2,26 @@ import { writable } from 'svelte/store';
 import { authService } from '$lib/services/authService';
 
 interface AuthState {
-    isLoggedIn: boolean;
-    userData: string | null;
+	isLoggedIn: boolean;
+	userData: string | null;
 }
 
 const initialState: AuthState = {
-    isLoggedIn: false,
-    userData: null,
+	isLoggedIn: false,
+	userData: null
 };
 
 const authStore = writable(initialState);
 
 // Function to update store based on login
 async function login(credentials: { username: string; password: string }) {
-  try {
-    const userData = await authService.login(credentials);
-    authStore.set({ isLoggedIn: true, userData });
-  } catch (error) {
-    console.error('Store login error:', error);
-    // Handle error, e.g., set an error state in the store
-  }
+	try {
+		const userData = await authService.login(credentials);
+		authStore.set({ isLoggedIn: true, userData });
+	} catch (error) {
+		console.error('Store login error:', error);
+		// Handle error, e.g., set an error state in the store
+	}
 }
 
 export default authStore;
