acc35dda9a1aa6c2272ddd378cf8a15b73c4d5f9
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-14 18:12:01 +0100

    main page formatting update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,5 +15,7 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen bg-black">
-	<div class="text-white text-4xl font-bold bg-black bg-opacity-50 p-5">{randomEmoji} BLOG IN PROGRESS {randomEmoji}</div>
+	<div class="text-white text-4xl font-bold bg-black bg-opacity-50 p-5">
+		{randomEmoji} BLOG IN PROGRESS {randomEmoji}
+	</div>
 </div>
