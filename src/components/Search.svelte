56b77ab2053ad3702af4bb70e0945a0149a0aedb
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-20 20:40:00 +0100

    added posts list, search and tags components

---
+++ src/components/Search.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { searchPosts } from '$lib/services/postsService';
+	let query = '';
+
+	async function handleSearch() {
+		const results = await searchPosts(query);
+		// Update the UI with the search results
+	}
+</script>
+
+<input bind:value={query} />
+<button on:click={handleSearch}>Search</button>
