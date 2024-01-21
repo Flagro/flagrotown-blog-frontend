aefdf1cf17fa9dab3088239f2bbb974e8793f0c1
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-21 15:36:19 +0100

    removed geopattern

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,14 @@
 <script>
 	import '../app.css';
-	import GeoPattern from 'geopattern';
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
-
-	// You can use a hard-coded string, or base it on some other property such as user input.
-	const pattern = GeoPattern.generate('fg it blog').toDataUrl();
-
-	// Define the background styles
-	const mainBodyBgStyle = `background-image: ${pattern};`;
 </script>
 
 <div class="header-bg">
 	<Header />
 </div>
 
-<div class="main-body-bg" style={mainBodyBgStyle}>
+<div class="main-body-bg">
 	<slot />
 </div>
 
