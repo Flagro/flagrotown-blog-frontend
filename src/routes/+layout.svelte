6ef9a9f0655df6a834fe1c92827488e1dd5fca8b
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-21 15:13:25 +0100

    geopattern ts fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,21 @@
 <script>
 	import '../app.css';
+	import GeoPattern from 'geopattern';
 	import Header from '../components/Header.svelte';
 	import Footer from '../components/Footer.svelte';
+
+	// You can use a hard-coded string, or base it on some other property such as user input.
+	const pattern = GeoPattern.generate('fg it blog').toDataUrl();
+
+	// Define the background styles
+	const mainBodyBgStyle = `background-image: ${pattern};`;
 </script>
 
 <div class="header-bg">
 	<Header />
 </div>
 
-<div class="main-body-bg">
+<div class="main-body-bg" style={mainBodyBgStyle}>
 	<slot />
 </div>
 
