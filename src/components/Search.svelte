c0c57880e721c06a4755afec3304d9305f647e7c
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-22 10:17:22 +0100

    added styling for the search bar

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -8,5 +8,12 @@
 	}
 </script>
 
-<input bind:value={query} />
-<button on:click={handleSearch}>Search</button>
+<div class="sticky top-0 z-10 flex items-center bg-blue-500/50 backdrop-blur-md rounded-xl border mx-4 w-3/4">
+	<input class="flex-1 rounded-l-xl bg-transparent px-4 py-2 placeholder-gray-400 text-gray-300 focus:outline-none" 
+		   bind:value={query} 
+		   placeholder="Search blogs..." />
+	<button class="bg-green-500 hover:bg-green-600 text-gray-300 rounded-r-xl px-4 py-2"
+			on:click={handleSearch}>
+		Go!
+	</button>
+</div>
