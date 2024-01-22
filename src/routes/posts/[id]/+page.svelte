a9488ee8999119b345647f3742fd40463bccd56a
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2024-01-22 20:07:45 +0100

    added dompurify for future safe upload html

--- src/routes/posts/[id]/+page.svelte
+++ src/routes/posts/[id]/+page.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { marked } from 'marked';
+	import DOMPurify from 'dompurify';
 	import { getPost } from '$lib/services/postsService';
 	import { page } from '$app/stores';
 	import type { PostText } from '$lib/types/Post';
 
 	let post: PostText | null = null;
 	let error: string | null = null;
+	let sanitizedHTML: string = '';
 
 	onMount(async () => {
 		const postId = $page.params.id;
 		try {
 			post = await getPost(postId);
+			if (post) {
+				const rawMarkdown = marked(post.text);
+				sanitizedHTML = DOMPurify.sanitize(await rawMarkdown);
+			}
 		} catch (e) {
 			error = e instanceof Error ? e.message : 'An unknown error occurred';
 		}
 	});
-
-	$: renderedText = post ? marked(post.text) : '';
 </script>
 
 <div class="page-body">
 	<div class="w-3/4">
 		{#if post}
 			<h1>{post.name}</h1>
-			<div>{@html renderedText}</div>
+			<!-- eslint-disable -->
+			<div>{@html sanitizedHTML}</div>
+			<!-- eslint-enable -->
 		{:else if error}
 			<p>{error}</p>
 		{:else}
